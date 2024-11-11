5d8ef5932dcc6809afa598c558adf9a321caed13
Oscar4n1ma <oscar4n1ma997@gmail.com>
2024-11-11 09:54:37 -0500

    Nuevas dependencias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { OtpModule } from './otp-service/otp.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Configuración para que esté disponible globalmente
+    }),
+    OtpModule,
+  ],
   controllers: [],
   providers: [],
 })
