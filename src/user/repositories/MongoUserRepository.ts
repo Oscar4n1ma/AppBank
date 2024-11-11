4b35949074d36fd9ec0276b94e9ac98b9f48ac85
KsR4z8v <79332655+KsR4z8v@users.noreply.github.com>
2024-11-11 10:04:20 -0500

    repositorio de usuario

--- src/user/repositories/MongoUserRepository.ts
+++ src/user/repositories/MongoUserRepository.ts
@@ -1,16 +1,21 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Injectable, NotFoundException } from '@nestjs/common';
 import UserRepository from '../interfaces/User.repository';
-import { Collection } from 'mongodb';
+import { Collection, ObjectId } from 'mongodb';
 import MongoClientDb from 'src/config/MongoClientDb';
-import { CreateClientDto } from '../dto/users.dto';
-import { UserStatus } from '../interfaces/User';
+import MongoAccountRepository from 'src/product/repositories/MongoAccountRepository';
+import MongoCardRepository from 'src/product/repositories/MongoCardRepository';
 
 @Injectable()
 export default class MongoUserRepository implements UserRepository {
   private readonly userCollection: Collection;
+  private readonly cardCollection: Collection;
+  private readonly accountCollection: Collection;
 
   constructor(private readonly mongoClientDb: MongoClientDb) {
     this.userCollection = this.mongoClientDb.db().collection('User');
+    this.cardCollection = this.mongoClientDb.db().collection('Account');
+    this.accountCollection = this.mongoClientDb.db().collection('Card');
   }
   async exist(email: string, username: string): Promise<boolean> {
     const respDb = await this.userCollection.findOne(
@@ -19,31 +24,76 @@ export default class MongoUserRepository implements UserRepository {
       },
       { projection: { _id: 1 } },
     );
-
     return respDb !== null;
   }
-
-  async createClient(user: CreateClientDto): Promise<string> {
-    const respDb = await this.userCollection.insertOne({
-      username: user.username,
-      email: user.email,
-      password: user.password,
-      usedPasswords: [user.password],
-      permissions: [],
-      state: UserStatus.DEACTIVATE,
-      type: 'client',
-      data: {
-        cc: user.cc,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        genre: user.genre,
-        monthlyIncome: user.monthlyIncome,
-        address: user.address,
-        maritalStatus: user.maritalStatus,
-        currentJob: user.currentJob,
-        dateBorn: user.dateBorn,
+  async get(id: string) {
+    const respDb = await this.userCollection.findOne(
+      {
+        _id: new ObjectId(id),
+        deletedAt: null,
       },
-    });
-    return respDb.insertedId.toString();
+      {
+        projection: {
+          password: 0,
+          _id: 0,
+          usedPasswords: 0,
+          deletedAt: 0,
+          permissions: 0,
+          roles: 0,
+        },
+      },
+    );
+    return respDb;
+  }
+
+  async create(user: unknown): Promise<string> {
+    const session = this.mongoClientDb.startSession();
+    try {
+      session.startTransaction();
+      const respDb = await this.userCollection.insertOne(user, { session });
+      await session.commitTransaction();
+      return respDb.insertedId.toString();
+    } catch (error) {
+      if (session.inTransaction()) {
+        await session.abortTransaction();
+      }
+    } finally {
+      await session.endSession();
+    }
+  }
+  async delete(id: string) {
+    const session = this.mongoClientDb.startSession();
+    try {
+      session.startTransaction();
+      const deletedAt: Date = new Date();
+
+      const userFound = await this.userCollection.findOneAndUpdate(
+        { _id: new ObjectId(id), deletedAt: null },
+        { $set: { deletedAt } },
+        { projection: { _id: 1 }, session },
+      );
+      if (!userFound) {
+        throw new NotFoundException('El usuario no existe.');
+      }
+      await this.accountCollection.updateOne(
+        { owner: new ObjectId(id) },
+        { $set: { deletedAt } },
+        { session },
+      );
+
+      await this.cardCollection.updateOne(
+        { owner: new ObjectId(id) },
+        { $set: { deletedAt } },
+        { session },
+      );
+      await session.commitTransaction();
+    } catch (error) {
+      if (session.inTransaction()) {
+        await session.abortTransaction();
+      }
+      throw error;
+    } finally {
+      await session.endSession();
+    }
   }
 }
