a1010f98644d8ed56094e89bbf2770a22d555738
Oscar4n1ma <oscar4n1ma997@gmail.com>
2024-11-11 09:55:31 -0500

    Se implementó el repositorio de autentificación

--- src/auth/MongoAuthRepository.ts
+++ src/auth/MongoAuthRepository.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Collection } from 'mongodb';
+import { Collection, ObjectId } from 'mongodb';
 import MongoClientDb from 'src/config/MongoClientDb';
 import AuthRepository from './interfaces/AuthRepository';
-import { LoginUserDto } from './dto/auth.dto';
-import { authUser } from './entities/auth.entity';
+import { AuthUser } from './entities/auth.entity';
 
 @Injectable()
 export default class MongoAuthRepository implements AuthRepository {
@@ -12,19 +11,72 @@ export default class MongoAuthRepository implements AuthRepository {
     this.userCollection = this.mongoClientDb.db().collection('User');
   }
 
-  async findCredentials(user: LoginUserDto): Promise<LoginUserDto> {
-    const credentials = await this.userCollection.findOne(
-      { username: user.username },
-      { projection: { username: 1, password: 1 } },
+  async findCredentials(crendentials: AuthUser) {
+    const respDb = await this.userCollection.findOne(
+      { username: crendentials.username, deletedAt: null },
+      {
+        projection: {
+          username: 1,
+          password: 1,
+          _id: 1,
+          email: 1,
+          roles: 1,
+          _2fa: 1,
+        },
+      },
     );
-    if (!credentials) {
+    if (!respDb) {
       return null;
     }
+    return respDb;
+  }
 
-    const credentialsFound: LoginUserDto = new authUser(
-      credentials.username,
-      credentials.password,
+  async findIdByEmail(email: string) {
+    const respDb = await this.userCollection.findOne(
+      { email, deletedAt: null },
+      { projection: { _id: 1 } },
     );
-    return credentialsFound;
+    if (!respDb) {
+      return null;
+    }
+    return respDb;
+  }
+
+  async findByField(
+    query: Record<string, any>,
+  ): Promise<{ _id: string } | null> {
+    const user = await this.userCollection.findOne(query, {
+      projection: { _id: 1 },
+    });
+
+    return user ? { _id: user._id.toString() } : null;
+  }
+
+  async updatePasswordById(userId: string, newPassword: string): Promise<void> {
+    const objectId = new ObjectId(userId);
+    const result = await this.userCollection.updateOne(
+      { _id: objectId },
+      { $set: { password: newPassword } },
+    );
+
+    if (result.modifiedCount === 0) {
+      throw new Error(
+        'No se pudo actualizar la contraseña, por favor intente de nuevo.',
+      );
+    }
+  }
+
+  async activateAccount(userId: string): Promise<void> {
+    const objectId = new ObjectId(userId);
+    const result = await this.userCollection.updateOne(
+      { _id: objectId },
+      { $set: { status: true } },
+    );
+
+    if (result.modifiedCount === 0) {
+      throw new Error(
+        'No se pudo activar la cuenta, por favor intente de nuevo.',
+      );
+    }
   }
 }
