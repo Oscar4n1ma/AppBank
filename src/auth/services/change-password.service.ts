7c71912be73eb949136df67a6356584b19b23f77
Oscar4n1ma <oscar4n1ma997@gmail.com>
2024-11-11 09:57:55 -0500

    Se añadió el servicio de cambio de contraseña con un token

---
+++ src/auth/services/change-password.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import MongoAuthRepository from '../MongoAuthRepository';
+import { verify } from 'jsonwebtoken';
+import { ChangePasswordDto } from '../dto/change-password.dto';
+import { hash, genSalt } from 'bcrypt';
+
+@Injectable()
+export class ChangePasswordService {
+  constructor(private readonly authRepository: MongoAuthRepository) {}
+  async use(changePassword: ChangePasswordDto, t: string) {
+    const { newPassword, confirmNewPassword } = changePassword;
+    const jwtSecret = process.env.SECRET_KEY_JWT;
+
+    if (newPassword != confirmNewPassword) {
+      return 'Las contraseñas no son iguales';
+    }
+    const { id: extractedId } = verify(t, jwtSecret) as { id: string };
+    const hashedPassword = await hash(newPassword, await genSalt());
+    await this.authRepository.updatePasswordById(extractedId, hashedPassword);
+    return 'Contraseña actualizada exitosamente';
+  }
+}
