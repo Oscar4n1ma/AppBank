92f6ebf743c0d9c799a4f16cb294790484e5f6f0
KsR4z8v <79332655+KsR4z8v@users.noreply.github.com>
2024-11-11 10:04:53 -0500

    dtos para usuarios tipo cliente, empresa y empleado

---
+++ src/user/dto/user-client.dto.ts
@@ -0,0 +1,59 @@
+import { IsNotEmpty, IsNumberString, IsString } from 'class-validator';
+
+export class CreateUserClientDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly dateBorn: string;
+
+  @IsNumberString()
+  @IsNotEmpty()
+  readonly phoneNumber: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly address: string;
+
+  @IsNumberString()
+  @IsNotEmpty()
+  readonly cardPin: string;
+
+  @IsNumberString()
+  @IsNotEmpty()
+  readonly cc: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly genre: string;
+
+  @IsNumberString()
+  @IsNotEmpty()
+  readonly monthlyIncome: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly maritalStatus: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly currentJob: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
