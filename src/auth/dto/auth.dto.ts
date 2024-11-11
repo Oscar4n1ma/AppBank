a943e1bb185d80506575ebedd0a788137cb93c0c
Oscar4n1ma <oscar4n1ma997@gmail.com>
2024-11-11 10:03:57 -0500

    Dto usado en el login (auth)

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class LoginUserDto {
   @IsString()
   @IsNotEmpty()
-  username: string;
+  user: string;
 
   @IsString()
   @IsNotEmpty()
