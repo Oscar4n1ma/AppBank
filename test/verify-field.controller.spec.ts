f49326062558f65fc43f57f444d100a3896e3c78
Oscar4n1ma <oscar4n1ma997@gmail.com>
2024-11-17 22:07:44 -0500

    Se añadio el test del verify field controller

---
+++ test/verify-field.controller.spec.ts
@@ -0,0 +1,80 @@
+import { UtilsModule } from 'src/utils/utils.module';
+import { Test } from '@nestjs/testing';
+import { MongoClientModule } from 'src/config/mongo-client.module';
+import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+
+import MongoAuthRepository from 'src/auth/MongoAuthRepository';
+import { VerifyFieldController } from 'src/auth/controllers/verify-field.controller';
+import { VerifyFieldService } from 'src/auth/services/verify-field.service';
+import { ErrorHandler } from 'src/utils/error-handler';
+
+describe('Autentificacion de usuarios', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [UtilsModule, MongoClientModule],
+      controllers: [VerifyFieldController],
+      providers: [MongoAuthRepository, VerifyFieldService, ErrorHandler],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it('Se verifica si existe el correo ingresado', async () => {
+    const email = 'oscar4n1ma997@gmail.com';
+    const response = await request(app.getHttpServer())
+      .post('/verify-field')
+      .send({
+        email,
+      })
+      .expect(200);
+    expect(response.body).toEqual({
+      error: false,
+      msg: 'El dato ingresado fue encontrado en este id',
+      data: {
+        exists: true,
+        id: expect.any(String),
+      },
+    });
+  });
+
+  it('Se verifica si existe la cedula ingresada', async () => {
+    const cc = '1006432255';
+    const response = await request(app.getHttpServer())
+      .post('/verify-field')
+      .send({
+        cc,
+      })
+      .expect(200);
+    expect(response.body).toEqual({
+      error: false,
+      msg: 'El dato ingresado fue encontrado en este id',
+      data: {
+        exists: true,
+        id: expect.any(String),
+      },
+    });
+  });
+
+  it('Deberia devolver un error al no existir el dato ingresado', async () => {
+    const cc = '111';
+    const response = await request(app.getHttpServer())
+      .post('/verify-field')
+      .send({
+        cc,
+      })
+      .expect(404);
+    expect(response.body).toEqual({
+      error: true,
+      msg: 'El dato ingresado no existe',
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
+    jest.clearAllTimers();
+  });
+});
