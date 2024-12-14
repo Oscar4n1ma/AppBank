194b4d4c914d41202bb012a6bc9381f521280e98
KsR4z8v <79332655+KsR4z8v@users.noreply.github.com>
2024-12-14 13:57:09 -0500

    restricciones de contraseña

--- src/auth/dto/change-password.dto.ts
+++ src/auth/dto/change-password.dto.ts
@@ -1,8 +1,19 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsStrongPassword } from 'class-validator';
 
 export class ChangePasswordDto {
   @IsNotEmpty()
   @IsString()
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minSymbols: 1,
+      minUppercase: 1,
+    },
+    {
+      message:
+        'La contraseña debe tener almenos un simbolo o una letra mayuscula',
+    },
+  )
   newPassword: string;
 
   @IsNotEmpty()
